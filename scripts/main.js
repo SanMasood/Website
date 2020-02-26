27fced4473300751fca81cce475e0abd3d10a75d
SanMasood <sanamasood.giki@gmail.com>
2020-02-26 19:01:15 +0000

    alertpop done

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,11 @@ function clrform(){
   }
 
   function submitform(){
-      document.getElementById('contactformid').alert();
+   // event.preventDefault();
+      alertpop(document.getElementById('formname').value, document.getElementById('mail').value, document.getElementById('msg').value);
+
   }
+function alertpop(value1, value2, value3){
+ return alert('You have entered:\n'+value1+'\n'+value2+'\n'+value3);
+
+}
